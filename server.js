6cd0fb91d45f5576f0f8896fcb7cfa72c8a3fd37
kbaitubayev <kbaitubayev@myseneca.ca>
2024-04-08 13:19:43 -0400

    mongodb to mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { connectToMongoDB, closeMongoDBConnection } = require('./mongodb');
+const { connectToMongoDB, closeMongoDBConnection } = require('./mongoDB');
 const routes = require('./routes/routes');
 const authRoutes = require('./routes/authRoutes');
 const passwordRoute = require('./routes/passwordRoute');
